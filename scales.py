b9a931f4746cac17332b847e2d0fc9b927a937ab
tfari <28794192+tfari@users.noreply.github.com>
2022-06-12 05:40:43 -0300

    Add display_scale and random_scale

--- scales.py
+++ scales.py
@@ -4,11 +4,15 @@ Display, listen, and manage a set of musical scales.
 import os
 import sys
 import json
+import random
+
 from typing import Optional
 
 import click
 
 
+# SET UP
+random.seed()
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_FILEPATH = f'{SCRIPT_PATH}/data.json'
 KEYS = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
@@ -19,8 +23,10 @@ SCALE_MAP = {}
 FACTORY_SCALE_MAP = {
     'Major': [2, 2, 1, 2, 2, 2, 1],
     'Minor': [2, 1, 2, 2, 1, 2, 2]
-    # Harmonic Minor
+    # Harmonic Minor TODO
 }
+
+# SET UP
 
 def __error_echo(err_msg: str, fatal: bool = False) -> None:
     """ Echo for errors, if fatal, exit application. Red foreground color."""
@@ -109,8 +115,41 @@ def list_scale_data(key_name: str = 'C') -> None:
                    f'Values: "{"-".join([str(sv) for sv in SCALE_MAP[scale_name]])}" - '
                    f'Example ({key_name.upper()}) - {"-".join(_scale_make(key_name_final, SCALE_MAP[scale_name]))}')
 
+@click.command('scale')
+@click.argument('SCALE_NAME')
+@click.argument('KEY_NAME', required=False, default='C')
+def display_scale(scale_name: str, key_name: str = 'C') -> None:
+    """ Display the Scale scale_name on the key KEY_NAME. If no KEY_NAME is passed, produce scale using C as root
+    key. """
+    global SCALE_MAP
+    scale_name_real = [sn for sn in SCALE_MAP.keys() if sn.upper() == scale_name.upper()]
+    if not scale_name_real:
+        __error_echo(f'Could not find scale: "{scale_name}"')
+    else:
+        scale_name_real = scale_name_real[0]
+        key_name_final = _key_translate(key_name)
+        if not key_name_final:
+            __error_echo(f'Could not understand key: {key_name}')
+        else:
+            __echo(f'"{scale_name_real}" scale in "{key_name.upper()}" : '
+                   f'{"-".join(_scale_make(key_name_final, SCALE_MAP[scale_name_real]))}')
+
+@click.command('random')
+@click.argument('KEY_NAME', required=False, default='C')
+def random_scale(key_name: str = 'C') -> None:
+    """ Display a random scale on the key KEY_NAME. If no KEY_NAME is passed, produce scale using C as root key. """
+    scale_name = random.choice(list(SCALE_MAP.keys()))
+    key_name_final = _key_translate(key_name)
+    if not key_name_final:
+        __error_echo(f'Could not understand key: {key_name}')
+    else:
+        __echo(f'"{scale_name}" scale in "{key_name.upper()}" : '
+               f'{"-".join(_scale_make(key_name_final, SCALE_MAP[scale_name]))}')
+
 
 if __name__ == '__main__':
     scales.add_command(list_scale_data)
     scales.add_command(restore_data)
+    scales.add_command(display_scale)
+    scales.add_command(random_scale)
     scales()
