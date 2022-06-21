0ab42ae2c4d3f4d0f49ac8f86db23276d4b5f6a1
tfari <28794192+tfari@users.noreply.github.com>
2022-06-21 04:25:16 -0300

    Refactor

--- scales.py
+++ scales.py
@@ -23,7 +23,7 @@ KEYS = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 FLAT_TRANSLATION_MAP = {'DB': 'C#', 'EB': 'D#', 'GB': 'F#', 'AB': 'G#', 'BB': 'A#'}
 
 
-SCALE_MAP = {}
+SCALE_MAP: dict = {}
 FACTORY_SCALE_MAP = {
     'Major': [2, 2, 1, 2, 2, 2, 1],
     'Minor': [2, 1, 2, 2, 1, 2, 2],
@@ -34,7 +34,7 @@ FACTORY_SCALE_MAP = {
     'Minor pentatonic': [3, 2, 2, 3]
 }
 
-# SET UP
+# Echoing
 
 def __error_echo(err_msg: str, fatal: bool = False) -> None:
     """ Echo for errors, if fatal, exit application. Red foreground color."""
@@ -50,38 +50,13 @@ def __echo(msg: str, fg_color: str = 'green') -> None:
     """ Normal echo, white foreground color by default. """
     click.secho(msg, fg=fg_color)
 
-#
-
-def _key_translate(key_name: str) -> Optional[str]:
-    """
-    Given key_name, return None if non exists, the translated key_name if it is a flat key, the key_name itself
-    otherwise.
-    """
-    key_name = key_name.upper()
-    if key_name in KEYS:
-        return key_name
-    elif key_name in FLAT_TRANSLATION_MAP.keys():
-        return FLAT_TRANSLATION_MAP[key_name]
-    else:
-        return None
-
-
-def _scale_make(root_key: str, scale_values: list[int]) -> list[str]:
-    """ Make a scale from a root_key and scale_values  """
-    index = KEYS.index(root_key)
-    scale = [KEYS[index]]
-    for sv in scale_values:
-        index += sv
-        scale.append(KEYS[index % len(KEYS)])
-
-    return scale
-#
+# #
 
 @click.group()
 def scales() -> None:
     """ Display, listen, and manage a set of musical scales. """
+    # Click entry-point
     __load_data()
-
 
 def __load_data() -> None:
     """ Load data.json file, if it doesn't exist, create a new one, if it is broken throw a fatal error. """
@@ -133,11 +108,10 @@ def display_scale(scale_name: str, key_name: str = 'C') -> None:
     """ Display the Scale scale_name on the key KEY_NAME. If no KEY_NAME is passed, produce scale using C as root
     key. """
     # TODO: Allow user to set ROOT_FREQ FOR __play
-    scale_name_real = [sn for sn in SCALE_MAP.keys() if sn.upper() == scale_name.upper()]
+    scale_name_real = __find_scale_by_name(scale_name)
     if not scale_name_real:
         __error_echo(f'Could not find scale: "{scale_name}"')
     else:
-        scale_name_real = scale_name_real[0]
         key_name_final = _key_translate(key_name)
         if not key_name_final:
             __error_echo(f'Could not understand key: {key_name}')
@@ -165,34 +139,24 @@ def random_scale(key_name: str = 'C') -> None:
 def add_scale(scale_name: str, scale_values: str) -> None:
     """ Add Scale. SCALE_VALUES should be a sequence of numbers, representing semitones,
     separated by '-' characters. """
-    wrong_chars = [sv for sv in scale_values if not sv.isnumeric() and sv != '-']
-    if wrong_chars:
-        __error_echo(f'Unexpected character(s) in scale_values: "{", ".join(wrong_chars)}"')
+    scale_name_real = __find_scale_by_name(scale_name)
+    if scale_name_real:
+        __error_echo(f'Scale name: "{scale_name}" already exists.')
     else:
-        scale_name_real = [sn for sn in SCALE_MAP.keys() if sn.upper() == scale_name.upper()]
-        if scale_name_real:
-            __error_echo(f'Scale name: "{scale_name}" already exists.')
-        else:
-            sv_ints = [sv for sv in scale_values.split('-')]
-            for sv in sv_ints:
-                if not sv or not sv.isnumeric():
-                    __error_echo(f'Invalid scale_values: "{scale_values}"', fatal=True)
-            SCALE_MAP[scale_name] = [int(sv) for sv in sv_ints]
-            __save_data(SCALE_MAP)
-            __info_echo(f'Added scale: "{scale_name}" with values: {sv_ints}')
+        final_values = __modify_scale_map(scale_name, scale_values)
+        __info_echo(f'Added scale: "{scale_name}" with values: {final_values}')
 
 @click.command('remove')
 @click.argument('SCALE_NAME')
 def remove_scale(scale_name: str) -> None:
     """ Remove scale with name SCALE_NAME. """
-    scale_name_real = [sn for sn in SCALE_MAP.keys() if sn.upper() == scale_name.upper()]
+    scale_name_real = __find_scale_by_name(scale_name)
     if not scale_name_real:
         __error_echo(f'Scale name: "{scale_name}" does not exist.')
     else:
-        scale_name_real = scale_name_real[0]
         SCALE_MAP.pop(scale_name_real)
         __save_data(SCALE_MAP)
-        __info_echo(f'Removed scale: "{scale_name}"')
+        __info_echo(f'Removed scale: "{scale_name_real}"')
 
 @click.command('edit')
 @click.argument('SCALE_NAME')
@@ -200,17 +164,60 @@ def remove_scale(scale_name: str) -> None:
 def edit_scale(scale_name: str, scale_values: str) -> None:
     """ Edit scale with name SCALE_NAME to values SCALE_VALUES. SCALE_VALUES should be a sequence of numbers,
         representing semitones, separated by '-' characters. """
-    scale_name_real = [sn for sn in SCALE_MAP.keys() if sn.upper() == scale_name.upper()]
+    scale_name_real = __find_scale_by_name(scale_name)
     if not scale_name_real:
         __error_echo(f'Scale name: "{scale_name}" does not exist.')
     else:
-        sv_ints = [sv for sv in scale_values.split('-')]
-        for sv in sv_ints:
-            if not sv or not sv.isnumeric():
-                __error_echo(f'Invalid scale_values: "{scale_values}"', fatal=True)
-        SCALE_MAP[scale_name] = [int(sv) for sv in sv_ints]
-        __save_data(SCALE_MAP)
-        __info_echo(f'Edited scale: "{scale_name}" with values: {sv_ints}')
+        final_values = __modify_scale_map(scale_name_real, scale_values)
+        __info_echo(f'Edited scale: "{scale_name_real}" with values: {final_values}')
+
+
+# Helpers
+
+def _key_translate(key_name: str) -> Optional[str]:
+    """
+    Given key_name, return None if non exists, the translated key_name if it is a flat key, the key_name itself
+    otherwise.
+    """
+    key_name = key_name.upper()
+    return key_name if key_name in KEYS else FLAT_TRANSLATION_MAP[key_name] if key_name in FLAT_TRANSLATION_MAP.keys() \
+        else None
+
+
+def _scale_make(root_key: str, scale_values: list[int]) -> list[str]:
+    """ Make a scale from a root_key and scale_values  """
+    relative_index = KEYS.index(root_key)
+    scale = [KEYS[relative_index]]  # Scales start from root_key
+    for sv in scale_values:
+        relative_index += sv
+        scale.append(KEYS[relative_index % len(KEYS)])
+
+    return scale
+
+
+def __find_scale_by_name(scale_name: str) -> Optional[str]:
+    """ Is scale_name a key in SCALE_MAP? Case-insensitive. Returns the name as it appears in SCALE_MAP, or None"""
+    for sn in SCALE_MAP.keys():
+        if sn.upper() == scale_name.upper():
+            return sn
+    return None
+
+
+def __modify_scale_map(scale_name: str, scale_values: str) -> list[str]:
+    """ Modify SCALE_MAP[scale_name] to have values scale_values, if valid. """
+    wrong_chars = [sv for sv in scale_values if not sv.isnumeric() and sv != '-']
+    if wrong_chars:
+        __error_echo(f'Unexpected character(s) in scale_values: "{", ".join(wrong_chars)}"', fatal=True)
+
+    sv_ints = [sv for sv in scale_values.split('-')]
+    for sv in sv_ints:
+        if not sv or not sv.isnumeric():
+            __error_echo(f'Invalid scale_values: "{scale_values}"', fatal=True)
+    SCALE_MAP[scale_name] = [int(sv) for sv in sv_ints]
+    __save_data(SCALE_MAP)
+    return sv_ints
+
+#
 
 def __play(root_note_key_name: str, scale_values: list[int], root_a_freq: int = 440) -> None:
     """ Play a scale """
